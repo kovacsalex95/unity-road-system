f1c98c3236ef6c0610dfecbbc2d6e90df50775f5
kovacsalex95 <kovacsalex95@gmail.com>
2023-03-09 00:44:54 +0100

    Added node creation and snapping

--- RoadSystem.cs
+++ RoadSystem.cs
@@ -1,12 +1,54 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
-
+using Random = UnityEngine.Random;
 
 namespace lxkvcs.UnityRoadSystem
 {
     public class RoadSystem : MonoBehaviour
     {
+        const float NEW_NODE_DISTANCE = 2f; // in units (m)
+        
+        
+        [SerializeField]
+        public bool enableSnapping = true;
+        [SerializeField]
+        public float snappingDistance = 0.25f;
+        
+        
+        private List<RoadNode> nodes = null;
+
+
+        public List<RoadNode> Nodes
+        {
+            get
+            {
+                if (nodes == null)
+                    nodes = new List<RoadNode>();
+
+                return nodes;
+            }
+        }
+
+
+        public void CreateNode()
+        {
+            Vector3 position = Vector3.zero;
+
+            if (Nodes.Count > 0)
+            {
+                position = nodes[nodes.Count - 1].Position;
+                float randomDegree = Random.Range(0f, 360f);
+                float randomX = Mathf.Sin(randomDegree * Mathf.Deg2Rad);
+                float randomY = Mathf.Cos(randomDegree * Mathf.Deg2Rad);
+                position += new Vector3(randomX * NEW_NODE_DISTANCE, 0, randomY * NEW_NODE_DISTANCE);
+            }
+
+            RoadNode node = new RoadNode(this as RoadSystem, SnapPointToGrid(position));
+            nodes.Add(node);
+        }
+
+
         public static RoadSystem FirstSystem()
         {
             RoadSystem[] objects = FindObjectsOfType<RoadSystem>();
@@ -17,5 +59,16 @@ namespace lxkvcs.UnityRoadSystem
         }
         
         
+        public Vector3 SnapPointToGrid(Vector3 point)
+        {
+            if (!enableSnapping || snappingDistance <= 0.001f)
+                return point;
+
+            float x = Mathf.Round(point.x / snappingDistance) * snappingDistance;
+            float y = Mathf.Round(point.y / snappingDistance) * snappingDistance;
+            float z = Mathf.Round(point.z / snappingDistance) * snappingDistance;
+
+            return new Vector3(x, y, z);
+        }
     }
 }
