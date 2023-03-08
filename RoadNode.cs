d59874f0e7785cae61447ecad2c802e672d42278
kovacsalex95 <kovacsalex95@gmail.com>
2023-03-09 00:43:38 +0100

    Added constructors

--- RoadNode.cs
+++ RoadNode.cs
@@ -8,5 +8,20 @@ namespace lxkvcs.UnityRoadSystem
         private RoadSystem system; 
         public RoadConnection[] Connections;
 
+        public RoadNode()
+        {
+            this.system = RoadSystem.FirstSystem();
+        }
+        
+        public RoadNode(RoadSystem system)
+        {
+            this.system = system;
+        }
+        
+        public RoadNode(RoadSystem system, Vector3 position)
+        {
+            this.position = position;
+            this.system = system;
+        }
     }
 }
