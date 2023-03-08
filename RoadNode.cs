379c67572076425b502e8520ee6665cc00ee6fe0
kovacsalex95 <kovacsalex95@gmail.com>
2023-03-09 00:42:47 +0100

    Added road node class

---
+++ RoadNode.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace lxkvcs.UnityRoadSystem
+{
+    public class RoadNode
+    {
+        private Vector3 position = Vector3.zero;
+        private RoadSystem system; 
+        public RoadConnection[] Connections;
+
+    }
+}
