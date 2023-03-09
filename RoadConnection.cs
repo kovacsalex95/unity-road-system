7b0464640478a568e33581828ca4305fd4c94698
kovacsalex95 <kovacsalex95@gmail.com>
2023-03-10 00:00:18 +0100

    Added both parts + recalculate distance function

--- RoadConnection.cs
+++ RoadConnection.cs
@@ -1,8 +1,16 @@
+using UnityEngine;
+
 namespace lxkvcs.UnityRoadSystem
 {
     public struct RoadConnection
     {
-        public RoadNode OtherNode;
+        public RoadNode NodeA;
+        public RoadNode NodeB;
         public float Distance;
+
+        public void RecalculateDistance()
+        {
+            Distance = Vector3.Distance(NodeA.Position, NodeB.Position);
+        }
     }
 }
