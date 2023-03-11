67180a6c1efce79d1d6251ffa61f45882bbadfd6
kovacsalex95 <kovacsalex95@gmail.com>
2023-03-11 19:37:51 +0100

    Added dynamic resource loader function

--- Resources.cs
+++ Resources.cs
@@ -8,6 +8,18 @@ namespace lxkvcs.UnityRoadSystem
     {
         private static GUIContent toolbarIcon = null;
         
+        private static string systemPath = null;
+
+
+        private static T LoadResource<T>(string path) where T : Object
+        {
+            if (systemPath == null)
+                systemPath = Path.GetDirectoryName(Util.GetMonoScriptPathFor(typeof(RoadSystem)));
+            
+            return AssetDatabase.LoadAssetAtPath<T>(systemPath +"/" + path);
+        }
+
+
         public static GUIContent ToolbarIcon
         {
             get
